be5f4250ab439fc1b74b484d49c96ea7322869ee
iamorlov <heiker.ru@gmail.com>
2018-11-28 22:17:25 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cats';
+export class AppComponent implements OnInit {
+  random:number = 2;
+  cat:string;
+  ngOnInit() {
+    setInterval(function(){
+      var random = Math.floor(Math.random()*3);
+      console.log(random);
+      return random;
+    }, 1000);
+    var cat = "wall-" + this.random +".jpg";
+  };
 }
