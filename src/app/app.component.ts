a9abd9e7915bb76319dbb37cb07a0f5a8f8a98d4
iamorlov <heiker.ru@gmail.com>
2018-11-29 21:39:00 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,13 @@ import { Component ,OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   random:number = Math.floor(Math.random()*11);
   cat:string = "wall-" + this.random +".jpg";
-  cat_wall:string = "/assets/walls/" + this.cat;
-  cats = document.getElementById("loveCats");
 
   constructor() {
-    console.log(this.cats);
     setInterval(() => {
       this.random = Math.floor(Math.random()*11);
       this.cat = "wall-" + this.random +".jpg";
-      this.cat_wall = "/assets/walls/" + this.cat;
-    }, 2000);
+    }, 4000);
   };
 
   ngOnInit() {};
-
-  openFullscreen() {
-    this.cats.requestFullscreen();
-  };
 }
