997d282e6a4287f4a9cfa1ed6a4656b9ac39a059
Vadim Orlov <vadym.orlov@globallogic.com>
2018-11-29 19:04:39 +0200

    cats update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,23 @@ import { Component ,OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  random:number = Math.floor(Math.random()*3);
+  random:number = Math.floor(Math.random()*11);
   cat:string = "wall-" + this.random +".jpg";
-  
+  cat_wall:string = "/assets/walls/" + this.cat;
+  cats = document.getElementById("loveCats");
+
   constructor() {
-    setInterval(function(){
-      this.random = Math.floor(Math.random()*3);
-      console.log(random);
+    console.log(this.cats);
+    setInterval(() => {
+      this.random = Math.floor(Math.random()*11);
+      this.cat = "wall-" + this.random +".jpg";
+      this.cat_wall = "/assets/walls/" + this.cat;
     }, 2000);
-  }
+  };
+
   ngOnInit() {};
+
+  openFullscreen() {
+    this.cats.requestFullscreen();
+  };
 }
