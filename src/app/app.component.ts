80cc9bd7c650b78f7fb0e3a339eb78eedf281283
iamorlov <heiker.ru@gmail.com>
2018-12-07 20:48:00 +0200

    Added new walls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  images: number = 240; // Count of cat walls ♥
+  images: number = 280; // Count of cat walls ♥
   random: number = Math.floor(Math.random() * this.images);
   random_new: number;
   cat: string = "wall-" + this.random + ".jpg";
