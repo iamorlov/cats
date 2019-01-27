968d7bc490a66f74db2f0c77c2b4f0671a8a6653
iamorlov <heiker.ru@gmail.com>
2019-01-27 22:30:27 +0200

    Added weather API, icons and style [BETA]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ControlsComponent } from './controls/controls.component';
@@ -10,7 +11,8 @@ import { ControlsComponent } from './controls/controls.component';
     ControlsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
