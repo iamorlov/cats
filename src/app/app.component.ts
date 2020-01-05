913da4837b9c96dcc2d4ed9ec5d8064b46f3b11e
iamorlov <heiker.ru@gmail.com>
2020-01-05 16:51:03 +0200

    Added new 40 cat walls! Changed color to the color of the year 2020, small fixes :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
 })
 
 export class AppComponent implements OnInit {
-  images = 440; // Count of cat walls ♥
+  images = 480; // Count of cat walls ♥
   random: number = Math.floor(Math.random() * this.images);
   random_new: number;
   cat: string = 'wall-' + this.random + '.jpg';
